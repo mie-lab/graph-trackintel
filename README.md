eb4466b208a23789652f068b1c92f95a8c560ab1
NinaWie <wnina@student.ethz.ch>
2022-01-05 10:12:50 +0100

    Initial commit - activity graph

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # graph-trackintel
 Tools for building and preprocessing location graphs
+
+## Installation:
+
+
+This repo can then be installed in editable mode. In an activated virtual environment, cd into this folder and run
+```
+pip install -e .
+```
+This will execute the `setup.py` file and install required dependencies
---
+++ setup.py
@@ -0,0 +1,36 @@
+"""Package installer."""
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+LONG_DESCRIPTION = ""
+if os.path.exists("README.md"):
+    with open("README.md") as fp:
+        LONG_DESCRIPTION = fp.read()
+
+scripts = []
+
+setup(
+    name="graph-trackintel",
+    version="0.0.1",
+    description="Tools for creating, processing and analyzing location graphs",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    author="MIE Lab",
+    author_email=("nwiedemann@ethz.ch"),
+    license="MIT",
+    url="https://github.com/mie-lab/graph-trackintel",
+    install_requires=[
+        "numpy", "scipy", "psycopg2", "networkx", "pyproj", "matplotlib",
+        "trackintel"
+    ],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    packages=find_packages("."),
+    python_requires=">=3.6",
+    scripts=scripts,
+)
